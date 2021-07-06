d74cf914911185b2cf8ee088b3108829982c2136
Rajeev <scraptorajeev@gmail.com>
2021-07-07 02:37:17 +0530

    Update boilerplatecode.ts

--- handlers/boilerplatecode.ts
+++ handlers/boilerplatecode.ts
@@ -1,11 +1,11 @@
 import { Handler, Context } from "aws-lambda";
-import { download } from "../services/compileGRPCStubs";
+import { generateNodejsBoilerplatecode } from "../services/compileGRPCStubs";
 
 export const create: Handler = async (
   event: { serviceId: string; orgId: string },
   context: Context
-): Promise<Context> => {
+): Promise<any> => {
   const { orgId, serviceId } = event;
-  await download(orgId, serviceId);
+  await generateNodejsBoilerplatecode(orgId, serviceId);
   return context.succeed({ status: true, message: "ok" });
 };
