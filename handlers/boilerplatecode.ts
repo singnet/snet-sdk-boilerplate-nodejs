b80ce3d15872f81b22cae62a73cf66c045e619eb
Rajeev <scraptorajeev@gmail.com>
2021-07-07 10:48:14 +0530

    Response interface added

--- handlers/boilerplatecode.ts
+++ handlers/boilerplatecode.ts
@@ -1,11 +1,11 @@
 import { Handler } from "aws-lambda";
 import { generateNodejsBoilerplatecode } from "../services/compileGRPCStubs";
-import { response } from "../utils/LambdaRequestResponse";
+import { response, Response } from "../utils/LambdaRequestResponse";
 
 export const create: Handler = async (event: {
   serviceId: string;
   orgId: string;
-}): Promise<object> => {
+}): Promise<Response> => {
   const { orgId, serviceId } = event;
   await generateNodejsBoilerplatecode(orgId, serviceId);
   return response("OK", 200);
